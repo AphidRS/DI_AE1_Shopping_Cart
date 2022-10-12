e67771fac644bac697173203e12ebfaa3f8e71f6
AphidRS <aphidrs@gmail.com>
2022-10-12 22:53:46 +0200

    New ticket frontend + exceptions cases

--- scripts/core.js
+++ scripts/core.js
@@ -8,6 +8,9 @@ var pvp;
 var cantidad;
 var totalCompra;
 var botonSumar;
+var faltaArticulo;
+var faltaPrecio;
+var ticket;
 const cesta = [];
 
 // =========================== //
@@ -28,10 +31,13 @@ window.addEventListener("load", ()=>{
 function initVars() {
 
 	articulo = document.getElementById("articulo");
+	faltaArticulo = document.getElementById("faltaArticulo");
 	pvp = document.getElementById("pvp");
+	faltaPrecio = document.getElementById("faltaPrecio");
 	cantidad = document.getElementById("cantidad");
 	totalCompra = document.getElementById("totalCompra");
 	articulosCarrito = document.getElementById("articulosCarrito");
+	ticket = document.getElementById("ticket");
 	botonSumar = document.getElementById("botonSumar");
 	articulo.focus();
 
@@ -59,23 +65,46 @@ function sumarAlCarrito() {
 		subtotal = totalCompra.value*1;
 		subtotal2 = parseInt(pvp.value*cantidad.value);
 
-		// Comprobamos que no hay campos a 0 y articulo no es nulo
-		if (pvp.value > 0 && cantidad.value > 0 && articulo.value != null){
-			
-			// Iteracion para introducir art. en array cesta 
-			for (var i = 0; i < cantidad.value; i++) {
-				cesta.push(articulo.value);
+		// Comprobamos que articulo no es nulo
+		if (articulo.value != null){
+
+			// Comprobamos que el pvp es mayor que 0
+			if (pvp.value > 0){
+
+				// Iteracion para introducir art. en array cesta 
+				for (var i = 0; i < cantidad.value; i++) {
+					cesta.push(articulo.value);
+				}
+				// Sumamos subtotal y totales y asignamos valor al campo totalCompra
+				totalCompra.value = subtotal + subtotal2;
+				articulosCarrito.value = cesta;
+				ticket.innerText = cesta;
+
+
+				// Borramos valores, quitamos warning y devolvemos foco a articulo
+				faltaArticulo.style.visibility = "hidden";
+				faltaPrecio.style.visibility = "hidden";
+				articulo.value = null;
+				pvp.value = "0";
+				cantidad.value = 1;
+				articulo.focus();
+
+			} else {
+
+				faltaPrecio.style.visibility = "visible";
+				pvp.value = 0;
+				pvp.focus();
+
 			}
-			// Sumamos subtotal y totales y asignamos valor al campo totalCompra
-			totalCompra.value = subtotal + subtotal2;
-			articulosCarrito.value = cesta;
+
+		} else {
+
+			faltaArticulo.style.visibility = "visible";
+			articulo.value = null;
+			articulo.focus();
+			articulo.style.border.color = "red";
 		}
 
-		// Borramos valores y devolvemos foco a articulo
-		articulo.value = null;
-		pvp.value = null;
-		cantidad.value = 1;
-		articulo.focus();
 }
 
 
