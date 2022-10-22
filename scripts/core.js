e341adc81f7ff9dec88ecbc3dc0b8e67f917b833
HectorT <hectabsan@gmail.com>
2022-10-23 00:57:13 +0200

    Agrego funcionalidades en  reset e imprimir. Modifico suma en carrito.

--- scripts/core.js
+++ scripts/core.js
@@ -6,6 +6,7 @@
 var articulo;
 var pvp;
 var cantidad;
+var articulosCarrito;
 var totalCompra;
 var botonSumar;
 var faltaArticulo;
@@ -20,7 +21,11 @@ var numTarjetaCvv;
 var numTarjetaLabel;
 var numTarjetaCvvLabel;
 var ticket;
+var condiciones;
+var botonImprimir;
+var botonReset;
 const cesta = [];
+
 
 // =========================== //
 // CARGA DE JS DESPUES DEL DOM //
@@ -29,7 +34,7 @@ const cesta = [];
 window.addEventListener("load", ()=>{
 	
 	initVars();
-
+	setListeners();
 });
 
 // ============================ //
@@ -62,6 +67,10 @@ function initVars() {
 	numTarjetaLabel = document.getElementById("numTarjetaLabel");
 	numTarjetaCvvLabel = document.getElementById("numTarjetaCvvLabel");
 
+	condiciones = document.getElementById("condiciones");
+	botonImprimir = document.getElementById("botonImprimir");	
+	botonReset = document.getElementById("botonReset");
+
 	// WARNINGS
 	faltaPrecio = document.getElementById("faltaPrecio");
 	faltaArticulo = document.getElementById("faltaArticulo");
@@ -78,9 +87,11 @@ function initVars() {
 
 function setListeners() {
 
-	botonSumar.addEventListener('click',sumarAlCarrito());
-	formaPago.addEventListener('change',opcionesPago());
-
+	botonSumar.addEventListener('click',sumarAlCarrito);
+	formaPago.addEventListener('change',opcionesPago);
+	botonImprimir.addEventListener('click',imprimirAlert);
+	condiciones.addEventListener('click', aceptarCondiciones);
+	botonReset.addEventListener('click',resetear);
 }
 
 // ======================== //
@@ -89,21 +100,29 @@ function setListeners() {
 
 function sumarAlCarrito() {
 	var subtotal;
-	var subtotal2
-	if (checkValues()){
-
+	var subtotal2;
+	articulosCarrito.value = "";
+	var isChecked = checkValues();	
+	if (isChecked) {	
+		const articuloObject = new Articulo(articulo.value, pvp.value, cantidad.value);
+				
 		// Convertimos valor de totalCompra a numero para evitar concatenacion de strings
 		subtotal = totalCompra.value*1;
-		subtotal2 = parseInt(pvp.value*cantidad.value);
-
-		// Iteracion para introducir art. en array cesta 
-		for (var i = 0; i < cantidad.value; i++) {
-			cesta.push(articulo.value,cantidad.value,pvp.value);
-		}
+		subtotal2 = parseFloat(articuloObject.pvp*articuloObject.cantidad);		
 		// Sumamos subtotal y totales y asignamos valor al campo totalCompra
 		totalCompra.value = subtotal + subtotal2;
-		articulosCarrito.value = cesta;
-		ticket.innerText = cesta;
+
+		// Introducir Articulo en array cesta 		
+		cesta.push(articuloObject);		
+		
+		//Introducimos valores al campo articulos del carrito
+		for(var i = 0; i < cesta.length; i++) {
+			if(i == (cesta.length - 1) || cesta.length == 1){// esto lo ponemos para que no meta la coma al final de la lista
+				articulosCarrito.value = articulosCarrito.value + cesta[i].nombre;
+			} else {
+				articulosCarrito.value = articulosCarrito.value + cesta[i].nombre + ", ";
+			} 
+		}				
 
 		// Borramos valores, quitamos warning y devolvemos foco a articulo
 		faltaArticulo.style.visibility = "hidden";
@@ -112,42 +131,57 @@ function sumarAlCarrito() {
 		pvp.value = 0;
 		cantidad.value = 1;
 		articulo.focus();
-	} 
+	}	
 }
 
+function Articulo(nombre, pvp, cantidad){
+	this.nombre = nombre;
+	this.pvp = pvp;
+	this.cantidad = cantidad;
+		
+	/*articuloObject.nombre = articulo.value;
+	articuloObject.pvp = pvp.value;
+	articuloObject.cantidad = cantidad.value;*/
+}
+	
+	// Verificamos que se cumplan las condiciones, retornando true si ambas funciones lo son.
+function checkValues() {	
+	var areChecked = isArticuloNotEmptyAndNaN() && isNotPvpCorrectValue();	
+	return areChecked;
+}
 
-function checkValues() {
-		pattern = /[0-9]/;
-		text = articulo.value;
+function isArticuloNotEmptyAndNaN(){
+	// Comprobamos que articulo no es nulo y que no es numerico sino 
+	// visibilizamos texto de error y reseteamos value y devolvemos foco a articulo
+	pattern = /[0-9]/;
+	text = articulo.value;
+	var isEmptyAndNaN = (text == "" || text.search(pattern) > -1);	
+	if (isEmptyAndNaN){
+		console.log("articulo mal");
+		faltaArticulo.style.visibility = "visible";
+		articulo.value = "";
+		articulo.focus();
+		articulo.style.border.color = "red";
+		setTimeout(restoreWarnings, 3000);		
+	}
+	return !isEmptyAndNaN;
+}
 
-		// Comprobamos que articulo no es nulo y que no es numerico sino 
-		// visibilizamos texto de error y reseteamos value y devolvemos foco a articulo
-		if (articulo.value == "" || text.search(pattern) == 0){
-			console.log("articulo mal");
-			faltaArticulo.style.visibility = "visible";
-			articulo.value = "";
-			articulo.focus();
-			articulo.style.border.color = "red";
-			setTimeout(restoreWarnings, 3000);
-			return false;
-		}
-
-		// Comprobamos que no es igual a 0 sino  
-		// vvisibilizamos texto de error y reseteamos value y devolvemos foco a precio
-		if (pvp.value == 0 ){
-			console.log("precio mal");
-			faltaPrecio.style.visibility = "visible";
-			pvp.value = 0;
-			pvp.focus();
-			setTimeout(restoreWarnings, 3000);
-			return false;
-
-		}
-	return true;
+function isNotPvpCorrectValue(){
+	/* Comprobamos que no es igual a 0 sino  
+	visibilizamos texto de error y reseteamos value y devolvemos foco a precio*/
+	var isPvpCorrectValue = pvp.value > 0;
+	if (!isPvpCorrectValue){
+		console.log("precio mal");
+		faltaPrecio.style.visibility = "visible";
+		pvp.value = 0;
+		pvp.focus();
+		setTimeout(restoreWarnings, 3000);
+	}
+	return isPvpCorrectValue;
 }
 
 function restoreWarnings(){
-
 	faltaArticulo.style.visibility = "hidden";
 	faltaPrecio.style.visibility = "hidden";
 
@@ -156,7 +190,6 @@ function restoreWarnings(){
 function opcionesPago (){
 
 	if (formaPago.value == "Efectivo") {
-
 		titularTarjetaLabel.style.visibility = "hidden";
 		titularTarjeta.style.visibility = "hidden";
 		numTarjetaLabel.style.visibility = "hidden";
@@ -168,10 +201,7 @@ function opcionesPago (){
 		efectivo.value = totalCompra.value;
 		console.log("soy efectivo");
 
-	} 
-
-	if (formaPago.value == "Tarjeta") {
-
+	} else if (formaPago.value == "Tarjeta") {
 		efectivoLabel.style.visibility = "hidden";
 		efectivo.style.visibility = "hidden";
 		titularTarjetaLabel.style.visibility = "visible";
@@ -182,10 +212,7 @@ function opcionesPago (){
 		numTarjetaCvv.style.visibility = "visible";
 		console.log("soy tarjeta");
 
-	}
-
-	if (formaPago.value == "default") {
-
+	} else {
 		titularTarjetaLabel.style.visibility = "hidden";
 		titularTarjeta.style.visibility = "hidden";
 		numTarjetaLabel.style.visibility = "hidden";
@@ -195,5 +222,40 @@ function opcionesPago (){
 		efectivoLabel.style.visibility = "hidden";
 		efectivo.style.visibility = "hidden";
 	}
+}
 
+function aceptarCondiciones(){
+	if (condiciones.checked){
+		botonImprimir.disabled = false;
+	} else{
+		botonImprimir.disabled = true;
+	}
+}
+
+function imprimirAlert(){
+	if(formaPago.value == "Efectivo" || formaPago.value == "Tarjeta") {
+		alert("Los articulos de mi carrito son: " + articulosCarrito.value + " y el precio total es: " + totalCompra.value + " Forma de pago: " + formaPago.value);
+	} else {
+		alert("Seleccione una forma de pago");
+	}
+}
+
+function resetear(){
+	articulo.value = "";	
+	pvp.value = "0";
+	cantidad.value = "1";
+	articulosCarrito.value = "";
+	totalCompra.value = "";	
+	formaPago.value = "";	
+	efectivo.value = "";
+	titularTarjeta.value = "";	
+	numTarjeta.value = "";
+	numTarjetaCvv.value = "";	
+	ticket.value = "";
+	condiciones.checked = false;
+	botonImprimir.disabled = true;
+	formaPago.selectedIndex = 0;
+	
+	cesta.length = 0;	
+	articulo.focus();
 }
